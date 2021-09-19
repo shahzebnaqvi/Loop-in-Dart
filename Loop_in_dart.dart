8e278dcc0831388fefb39b9e069c865d49c2dddf
shahzebnaqvi <50788207+shahzebnaqvi@users.noreply.github.com>
2021-09-19 14:22:00 +0500

    Practice

---
+++ Loop_in_dart.dart
@@ -0,0 +1,55 @@
+void main() {
+  
+  
+  print("for loop");
+for(var i=0;i<5;i ++ ){
+  print(i);
+}
+
+  
+  
+  print("while loop");
+
+  var i=0; 
+  while(i<5){
+    print(i);
+    i++;
+  }
+  
+  
+  
+  
+  
+  
+  print("Class Practice");
+  var array = [1,2,3,4,5,6,7,8,9,10];
+  for(var i=0;i<5;i ++ ){
+  print(array[i]);
+}
+  print("For loop for list");
+  for (var a in array){
+    print("array value $a");
+    print("b");
+  }
+  
+  
+  print("Do While loop");
+  
+  
+  i = 0; 
+   do { 
+      print(i); 
+      i++; 
+   }
+   while(i<10); 
+  
+  
+  
+  print("Table of 7888");  
+  print("----------------");
+  for(var i = 10 ; i <=130; i++){
+    var table = 7888;
+    print("$table * $i = ${table *i}");
+  }
+  
+}
